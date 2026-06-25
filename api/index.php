15fe01d541bc1d205bf3b8086cde819c9c00a920
latihanCommit <yobopi8143@inupup.com>
2026-06-25 17:13:48 +0800

    deploy

--- api/index.php
+++ api/index.php
@@ -1,2 +1,15 @@
 <?php
+
+// Memaksa Laravel membuang semua file cache, log, dan sesi ke folder /tmp
+putenv('APP_CONFIG_CACHE=/tmp/config.php');
+putenv('APP_EVENTS_CACHE=/tmp/events.php');
+putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
+putenv('APP_ROUTES_CACHE=/tmp/routes.php');
+putenv('APP_SERVICES_CACHE=/tmp/services.php');
+putenv('VIEW_COMPILED_PATH=/tmp');
+putenv('LOG_CHANNEL=stderr');
+putenv('CACHE_STORE=array');
+putenv('SESSION_DRIVER=cookie');
+
+// Menjalankan aplikasi Laravel
 require __DIR__ . '/../public/index.php';
